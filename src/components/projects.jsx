aa77e56512051e5a8176d9c3e556856a26ae67b9
suthardhaval24 <suthardhaval24@gmail.com>
2020-06-23 16:02:47 -0400

    commit

--- src/components/projects.jsx
+++ src/components/projects.jsx
@@ -16,7 +16,7 @@ export default class Projects extends Component {
 								<div className="project" style={{backgroundImage: 'url(images/aws.png)'}}>
 									<div className="desc">
 										<div className="con">
-											<h3><a href="#">Billing and Invocing Web Services - AWS Deployment</a></h3>
+											<h3>Billing and Invocing Web Services - AWS Deployment</h3>
 											<span>Technologies : Java, Spring Boot, Amazon S3, SQS, SNS, DynamoDB, Amazon RDS, EC2, CircleCI, AWS CodDeploy</span>
 											<p className="icon">
 												<span><a href="https://github.com/suthardhaval24/webapp"><i class="icon-github"></i></a></span>
@@ -29,7 +29,7 @@ export default class Projects extends Component {
 								<div className="project" style={{backgroundImage: 'url(images/packer.png)'}}>
 									<div className="desc">
 										<div className="con">
-											<h3><a href="#">Amazon Machine Image</a></h3>
+											<h3>Amazon Machine Image</h3>
 											<span>Technologies: Hashicorp Packer, Shell(Bash) Script, CircleCI, GIT </span>
 											<p className="icon">
 											<span><a href="https://github.com/suthardhaval24/ami"><i class="icon-github"></i></a></span>
@@ -42,7 +42,7 @@ export default class Projects extends Component {
 								<div className="project" style={{backgroundImage: 'url(images/lamda.png)'}}>
 									<div className="desc">
 										<div className="con">
-											<h3><a href="#">Serverless Computing: AWS Lambda</a></h3>
+											<h3>Serverless Computing: AWS Lambda</h3>
 											<span>Technologies: AWS Lambda, CircleCI, DynamoDB, Java, SQS, SNS</span>
 											<p className="icon">
 											<span><a href="https://github.com/suthardhaval24/serverless"><i class="icon-github"></i></a></span>
@@ -55,7 +55,7 @@ export default class Projects extends Component {
 								<div className="project" style={{backgroundImage: 'url(images/cloudformation.png)'}}>
 									<div className="desc">
 										<div className="con">
-											<h3><a href="work.html">AWS Cloud Infrastructure: Automation</a></h3>
+											<h3>AWS Cloud Infrastructure: Automation</h3>
 											<span>Technologies: CloudFormation, Terraform, Shell Scripting, Git</span>
 											<p className="icon">
 											<span><a href="https://github.com/suthardhaval24/infrastructure"><i class="icon-github"></i></a></span>
@@ -68,7 +68,7 @@ export default class Projects extends Component {
 								<div className="project" style={{backgroundImage: 'url(images/spring.png)'}}>
 									<div className="desc">
 										<div className="con">
-											<h3><a href="#">Event Booking Portal</a></h3>
+											<h3>Event Booking Portal</h3>
 											<span>Technologies: Spring MVC, Java, Hibernate, JDBC, MYSQL, HTML, CSS, JavaScript</span>
 											<p className="icon">
 											<span><a href="https://github.com/suthardhaval24/EventBookingSystem"><i class="icon-github"></i></a></span>
@@ -81,7 +81,7 @@ export default class Projects extends Component {
 								<div className="project" style={{backgroundImage: 'url(images/music.png)'}}>
 									<div className="desc">
 										<div className="con">
-											<h3><a href="#">Music Mood Analyzer</a></h3>
+											<h3>Music Mood Analyzer</h3>
 											<span>Technologies: Flask, REST APIs, Python, IBM NLP toolkit, Google Traslator APIs, Heroku, Docker</span>
 											<p className="icon">
 											<span><a href="https://github.com/suthardhaval24/MusicMoodAnalyzer"><i class="icon-github"></i></a></span>
@@ -93,7 +93,7 @@ export default class Projects extends Component {
 						</div>
 						<div className="row">
 							<div className="col-md-12 animate-box">
-								<p><a href="https://github.com/suthardhaval24" className="btn btn-primary btn-lg btn-load-more">Load more <i class="icon-arrow-right"></i></a></p>
+								<p><a href="https://github.com/suthardhaval24" className="btn btn-primary btn-lg btn-load-more">See all project on Github <i class="icon-arrow-right"></i></a></p>
 							</div>
 						</div>
 					</div>
