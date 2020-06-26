4c8e875bbbd0615fe075024824aa242003dd46b8
Deployment Bot (from Travis CI) <deploy@travis-ci.org>
2020-06-26 21:36:11 +0000

    Deploy suthardhaval24/portfolio to github.com/suthardhaval24/portfolio.git:gh-pages

---
+++ precache-manifest.0b7fb86c6793a7518c2e5d23ab4ac87c.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "bae7cfe91d128ce13b135f13aa53dbd5",
+    "url": "/portfolio/index.html"
+  },
+  {
+    "revision": "b97743b1076a4f90e2bf",
+    "url": "/portfolio/static/js/2.8f18329a.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/portfolio/static/js/2.8f18329a.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "7ca788304a4b811f3705",
+    "url": "/portfolio/static/js/main.2a9dca70.chunk.js"
+  },
+  {
+    "revision": "7a6cc99bd8f08f9fa6ec",
+    "url": "/portfolio/static/js/runtime-main.209c2d7e.js"
+  }
+]);
